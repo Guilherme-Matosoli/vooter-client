059921cc1ca36ca51e0d743ab0b34a6c946df0d7
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-31 06:18:58 -0300

    implements getOptions function :tada:

--- src/app/i18n/settings.ts
+++ src/app/i18n/settings.ts
@@ -1,3 +1,15 @@
 export const fallbackLng = "pt";
 export const languages = ["en", fallbackLng];
-export const cookieName = "i18language";
+export const cookieName = "i18next";
+export const defaultNS = "translation";
+
+export function getOptions(lng = fallbackLng, ns = defaultNS) {
+  return {
+    supportedLngs: languages,
+    fallbackLng,
+    lng,
+    fallbackNS: defaultNS,
+    defaultNS,
+    ns
+  };
+};
