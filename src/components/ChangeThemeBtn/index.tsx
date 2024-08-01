fb9fcac3ae67f701f842b116ac3dc79940b55440
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-01 13:41:47 -0300

    configure changethemebtn to change theme onclick :fire:

---
+++ src/components/ChangeThemeBtn/index.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export function ChangeThemeBtn() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    const darkPrefers = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (darkPrefers && !localTheme) {
+      setTheme("dark");
+
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark")
+      return;
+    };
+
+    if (localTheme) setTheme(localTheme);
+    if (localTheme == "dark") document.documentElement.classList.add("dark");
+  }, [localStorage.getItem("theme")]);
+
+  const changeTheme = () => {
+    const localTheme = localStorage.getItem("theme");
+
+    document.documentElement.classList.toggle("dark");
+
+    if (localTheme == "light") {
+      setTheme("dark")
+      localStorage.setItem("theme", "dark")
+      return
+    };
+
+    localStorage.setItem("theme", "light");
+    setTheme("light");
+  };
+
+  return (
+    <button
+      className="flex items-center justify-center p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-900 transition"
+      onClick={changeTheme}
+    >
+      <Image
+        src={theme == "light" ? "/icons/sun.svg" : "/icons/moon.svg"}
+        alt={theme == "light" ? "Sol" : "Lua"}
+        width={25}
+        height={25}
+      />
+    </button>
+  )
+}
