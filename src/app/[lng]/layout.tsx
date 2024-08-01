5c8b379db097b5d77f71294bf5ac092e65029e6e
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-31 21:09:48 -0300

    import google fonts in root layout head :robot:

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +17,11 @@ export default function RootLayout({ children, params: { lng } }: RootLayoutProp
 
   return (
     <html lang={lng}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
