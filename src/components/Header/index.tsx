0c5ffb03997aa429860b67a0650f9a563924e0cc
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-31 21:07:52 -0300

    initialize header component :rocket:

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+
+export function Header() {
+  return (
+    <header className="w-full bg-white dark:bg-black py-6 px-5 rounded-lg">
+      <h1 className="font-main font-bold text-2xl">
+        <a href="/">
+          Votter
+        </a>
+      </h1>
+    </header>
+  )
+}
