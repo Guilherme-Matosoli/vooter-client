66ac82997818e3d027e70b13b870f8c40f406392
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-01 16:20:46 -0300

    add dynamic translations (remove hard coded strings) :robot:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import { ChangeThemeBtn } from "../ChangeThemeBtn";
+import { useTranslation } from "@/app/i18n";
 
-export function Header() {
+interface HeaderProps {
+  lng: string
+};
+
+export function Header({ lng }: HeaderProps) {
+  const { t } = useTranslation(lng, "header");
   return (
     <header className="flex justify-between items-center w-full bg-white dark:bg-black p-5 rounded-lg">
       <h1 className="font-main font-bold text-2xl text-light dark:text-dark">
@@ -11,8 +17,12 @@ export function Header() {
       </h1>
 
       <nav className="flex gap-8">
-        <Link className="link" href="/">Home</Link>
-        <Link className="link" href="/">Criar enquete</Link>
+        <Link className="link" href="/">
+          {t("home")}
+        </Link>
+        <Link className="link" href="/">
+          {t("newPoll")}
+        </Link>
       </nav>
 
       <ChangeThemeBtn />
