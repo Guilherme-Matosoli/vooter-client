4a6f7b283e6668ce313879931e767a4a2082e5db
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-01 16:13:14 -0300

    configure Links and styles for then :tada:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,21 @@
+import Link from "next/link";
+import { ChangeThemeBtn } from "../ChangeThemeBtn";
 
 export function Header() {
   return (
-    <header className="w-full bg-white dark:bg-black py-6 px-5 rounded-lg">
-      <h1 className="font-main font-bold text-2xl">
+    <header className="flex justify-between items-center w-full bg-white dark:bg-black p-5 rounded-lg">
+      <h1 className="font-main font-bold text-2xl text-light dark:text-dark">
         <a href="/">
           Votter
         </a>
       </h1>
 
-      <nav>
+      <nav className="flex gap-8">
+        <Link className="link" href="/">Home</Link>
+        <Link className="link" href="/">Criar enquete</Link>
       </nav>
 
-      <button>
-        themeChange
-      </button>
+      <ChangeThemeBtn />
     </header>
   )
 }
