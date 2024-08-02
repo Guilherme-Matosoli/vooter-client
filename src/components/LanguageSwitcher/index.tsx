57193bcef3b5acc5cda6a5c1c0a5f8475cea8594
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-02 17:15:15 -0300

    :lipstick: style(components/LanguageSwitcher): Enhance LanguageSwitcher UI with toggle functionality and improved styles.

--- src/components/LanguageSwitcher/index.tsx
+++ src/components/LanguageSwitcher/index.tsx
@@ -1,14 +1,31 @@
-export function LanguageSwitcher() {
-  return (
-    <section className="flex gap-2 items-center justify-center">
-      <button className="w-[30px] h-[30px] rounded-full  overflow-hidden">
-        <img
-          src="/brazilFlag.svg"
-          alt="Language"
-        />
-      </button>
+"use client";
+import { useState } from "react";
 
-      <div className="border-[4px] border-t-dark border-l-dark border-r-black border-b-black w-1 h-1 rotate-45"></div>
-    </section>
+export function LanguageSwitcher() {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className="relative">
+      <section className="flex gap-2 items-center justify-center group">
+        <button onClick={() => setActive(!active)} className="w-[30px] h-[30px] rounded-full  overflow-hidden">
+          <img
+            src="/UM.svg"
+            alt="Language"
+            className="object-fill"
+          />
+        </button>
+
+        <div className={`border-[4px] border-t-transparent border-l-transparent
+border-r-black border-b-black w-1 h-1 rotate-45 dark:border-r-white dark:border-b-white group-hover:rotate-[225deg] transition`}>
+        </div>
+      </section>
+
+      {
+        active && (
+          <section className="absolute top-full rounded-xl bg-white w-52 min-h-10 border border-black -translate-x-1/2">
+          </section>
+        )
+      }
+    </div>
   );
 };
