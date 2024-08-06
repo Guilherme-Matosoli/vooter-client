4b8da2d651a357d942053942ba9c441e593cd0f8
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-06 16:09:11 -0300

    :recycle: refactor(components/Header): Refactor `Header` component to use `DesktopHeader` and `MobileHeader` for better structure and maintainability.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,15 @@
-import Link from "next/link";
-import { useTranslation } from "@/app/i18n";
-import { LanguageSwitcher } from "../LanguageSwitcher";
-import { ThemeSwitcher } from "../ThemeSwitcher";
+import { DesktopHeader } from "./DesktopHeader"
+import { MobileHeader } from "./MobileHeader"
 
-interface HeaderProps {
+export interface HeaderProps {
   lng: string
 };
 
 export function Header({ lng }: HeaderProps) {
-  const { t } = useTranslation(lng, "header");
   return (
-    <header className="flex justify-between items-center w-full bg-white dark:bg-black py-4 px-5 rounded-lg">
-      <h1 className="font-main font-bold text-2xl text-light dark:text-dark">
-        <a href="/">
-          Votter
-        </a>
-      </h1>
-
-      <nav className="flex gap-8">
-        <Link className="link" href="/">
-          {t("home")}
-        </Link>
-        <Link className="link" href="/">
-          {t("newPoll")}
-        </Link>
-      </nav>
-
-      <section className="flex items-center gap-2">
-        <LanguageSwitcher />
-        <ThemeSwitcher />
-      </section>
-    </header>
+    <>
+      <DesktopHeader lng={lng} />
+      <MobileHeader lng={lng} />
+    </>
   )
 }
