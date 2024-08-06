ed8841e57a1a4f84f564b77f87bd11ef7fc21b56
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-06 20:09:25 -0300

    make options just appear when open is true :robot:

--- src/components/Header/MobileHeader.tsx
+++ src/components/Header/MobileHeader.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { HeaderProps } from ".";
+import { HeaderCTAs } from "./HeaderCTAs";
 
 export function MobileHeader({ lng }: HeaderProps) {
   const [open, setOpen] = useState(false);
 
   return (
-    <section className="flex items-center h-full">
+    <section className="flex items-center h-full relative">
       <button
         onClick={() => setOpen(!open)}
         className="flex items-center justify-center h-[45px] w-[45px] rounded-full hover:bg-gray-200"
@@ -13,8 +14,8 @@ export function MobileHeader({ lng }: HeaderProps) {
         <span className={`menuIcon ${open && "open"}`}></span>
       </button>
 
-      <div>
-
+      <div className={`${open ? "flex" : "hidden"} flex-col absolute right-full top-full w-52 h-20 bg-red-200`}>
+        <HeaderCTAs lng={lng} />
       </div>
     </section>
   );
