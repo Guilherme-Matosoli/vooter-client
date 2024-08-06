739658cfa67c98e8f812105ea05d68d286463c85
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-06 16:15:51 -0300

    separate header cta's in an component :tada:

---
+++ src/components/Header/HeaderCTAs.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { LanguageSwitcher } from "../LanguageSwitcher";
+import { ThemeSwitcher } from "../ThemeSwitcher";
+import { HeaderProps } from ".";
+import { useTranslation } from "@/app/i18n";
+
+export function HeaderCTAs({ lng }: HeaderProps) {
+  const { t } = useTranslation(lng, "header");
+  return (
+    <>
+      <nav className="flex gap-8">
+        <Link className="link" href="/">
+          {t("home")}
+        </Link>
+        <Link className="link" href="/">
+          {t("newPoll")}
+        </Link>
+      </nav>
+
+      <section className="flex items-center gap-2">
+        <LanguageSwitcher />
+        <ThemeSwitcher />
+      </section>
+    </>
+  )
+}
