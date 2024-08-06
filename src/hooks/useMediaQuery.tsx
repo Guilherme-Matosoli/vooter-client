6d502e1c9055d364d8445459d03659042b272e45
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-06 17:02:23 -0300

    remove event listenet on unmount component :tada:

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -3,17 +3,19 @@
 import { useCallback, useEffect, useState } from "react";
 
 export function useMediaQuery(width: number) {
-  const [matches, setMatches] = useState(false);
+  const [matches, setMatches] = useState<boolean>();
 
   const handleChangeSize = useCallback((e: MediaQueryListEvent) => {
     e.matches ? setMatches(true) : setMatches(false);
   }, []);
 
   useEffect(() => {
-    const media = window.matchMedia(`(min-width: ${width}px)`);
+    const media = window.matchMedia(`(max-width: ${width}px)`);
     media.addEventListener("change", handleChangeSize);
 
     media.matches && setMatches(true);
+
+    return () => media.removeEventListener("change", handleChangeSize);
   }, []);
 
   return matches;
