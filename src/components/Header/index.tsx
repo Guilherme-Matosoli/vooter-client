03dd275b9cc545be59f91f048cea1223292e4430
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-06 16:54:44 -0300

    implements conditional render to show appropriated header (mobile or desktop) :fire:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,29 @@
-import { HeaderCTAs } from "./HeaderCTAs"
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { HeaderCTAs } from "./HeaderCTAs";
+import { MobileHeader } from "./MobileHeader";
 
 export interface HeaderProps {
   lng: string
 };
 
 export function Header({ lng }: HeaderProps) {
+  const matches = useMediaQuery(768);
+
   return (
-    <header className="hidden md:flex justify-between items-center w-full bg-white dark:bg-black py-4 px-5 rounded-lg">
+    <header className="flex justify-between items-center w-full bg-white dark:bg-black py-4 px-5 rounded-lg">
       <h1 className="font-main font-bold text-2xl text-light dark:text-dark">
         <a href="/">
           Votter
         </a>
       </h1>
 
-      <HeaderCTAs lng={lng} />
+      {
+        matches
+          ?
+          <HeaderCTAs lng={lng} />
+          :
+          <MobileHeader />
+      }
     </header>
   )
 }
