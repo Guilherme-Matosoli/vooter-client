d77649725274a115abbdef71cde71ded369de76f
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-06 20:19:37 -0300

    add function that handle with user click, if user click outside header, the modal will close :robot:

--- src/components/Header/MobileHeader.tsx
+++ src/components/Header/MobileHeader.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { HeaderProps } from ".";
 import { HeaderCTAs } from "./HeaderCTAs";
 
 export function MobileHeader({ lng }: HeaderProps) {
   const [open, setOpen] = useState(false);
 
+  const elementRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    function handleClick(e: MouseEvent) {
+      const clickInside = elementRef.current?.contains(e.target as Node);
+
+      if (!clickInside) setOpen(false);
+    };
+
+    window.addEventListener("click", handleClick);
+
+    return () => window.removeEventListener("click", handleClick);
+  }, []);
+
   return (
-    <section className="flex items-center h-full relative">
+    <section ref={elementRef} className="flex items-center h-full relative">
       <button
         onClick={() => setOpen(!open)}
         className="flex items-center justify-center h-[45px] w-[45px] rounded-full hover:bg-gray-200"
@@ -14,7 +27,7 @@ export function MobileHeader({ lng }: HeaderProps) {
         <span className={`menuIcon ${open && "open"}`}></span>
       </button>
 
-      <div className={`${open ? "flex" : "hidden"} flex-col absolute right-full top-full w-52 h-20 bg-red-200`}>
+      <div className={`${open ? "flex" : "hidden"} flex-col absolute right-full top-full w-52 h-auto bg-red-200 p-5`}>
         <HeaderCTAs lng={lng} />
       </div>
     </section>
