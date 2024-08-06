9d50a57099773ca9648961224517909bc5e1c7b1
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-06 20:03:11 -0300

    stylish menu button :tada:

--- src/components/Header/MobileHeader.tsx
+++ src/components/Header/MobileHeader.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 export function MobileHeader() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <section className="flex items-center">
-      <button>
-        <span className="menuIcon"></span>
+    <section className="flex items-center h-full">
+      <button
+        onClick={() => setOpen(!open)}
+        className="flex items-center justify-center h-[45px] w-[45px] rounded-full hover:bg-gray-200"
+      >
+        <span className={`menuIcon ${open && "open"}`}></span>
       </button>
     </section>
   );
