35f600e31b3802391836f4842845770c1465c656
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-07 16:07:30 -0300

    add function that handle with user click, if user click outside, the modal will close :robot:

--- src/components/LanguageSwitcher/index.tsx
+++ src/components/LanguageSwitcher/index.tsx
@@ -1,13 +1,26 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function LanguageSwitcher() {
-  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const elementRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    function handleClick(e: MouseEvent) {
+      const clickInside = elementRef.current?.contains(e.target as Node);
+
+      if (!clickInside) setOpen(false);
+    };
+
+    window.addEventListener("click", handleClick);
+
+    return () => window.removeEventListener("click", handleClick);
+  }, []);
 
   return (
-    <div className="relative">
+    <div className="relative" ref={elementRef}>
       <section className="flex gap-2 items-center justify-center group">
-        <button onClick={() => setActive(!active)} className="w-[30px] h-[30px] rounded-full  overflow-hidden">
+        <button onClick={() => setOpen(!open)} className="w-[30px] h-[30px] rounded-full  overflow-hidden">
           <img
             src="/usaFlag.svg"
             alt="Language"
@@ -21,7 +34,7 @@ border-r-black border-b-black w-1 h-1 rotate-45 dark:border-r-white dark:border-
       </section>
 
       {
-        active && (
+        open && (
           <section className="absolute top-full rounded-xl bg-white w-52 min-h-10 border border-black -translate-x-1/2">
           </section>
         )
