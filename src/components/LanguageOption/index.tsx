9de72a3c287fb92e5aa1004f4887d5e83563eb70
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-08 21:09:20 -0300

    add language dynamic props :rocket:

--- src/components/LanguageOption/index.tsx
+++ src/components/LanguageOption/index.tsx
@@ -1,19 +1,24 @@
 import { ButtonHTMLAttributes } from "react";
 
-interface LanguageOptionProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-
+interface Language {
+  name: string,
+  flag: string
 };
 
-export function LanguageOption({ ...rest }: LanguageOptionProps) {
+interface LanguageOptionProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  language: Language
+};
+
+export function LanguageOption({ language, ...rest }: LanguageOptionProps) {
   return (
     <button {...rest} className="w-full h-8 hover:bg-gray-200 transition flex items-center gap-3 p-3">
       <img
-        src="/flags/en.svg"
+        src={language.flag}
         className="w-6 h-6 rounded-full"
       />
 
       <span>
-        English
+        {language.name}
       </span>
     </button>
   );
