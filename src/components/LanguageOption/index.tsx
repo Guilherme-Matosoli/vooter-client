17157f640a285ed5367337381ce3a70df6fd0291
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-08 21:13:24 -0300

    get language desc by i18n lagnuages config :rocket:

--- src/components/LanguageOption/index.tsx
+++ src/components/LanguageOption/index.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from "@/app/i18n";
+import i18next from "i18next";
 import { ButtonHTMLAttributes } from "react";
 
 interface Language {
@@ -10,6 +12,7 @@ interface LanguageOptionProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 };
 
 export function LanguageOption({ language, ...rest }: LanguageOptionProps) {
+  const { t } = useTranslation(i18next.language, "languages");
   return (
     <button {...rest} className="w-full h-8 hover:bg-gray-200 transition flex items-center gap-3 p-3">
       <img
@@ -18,7 +21,7 @@ export function LanguageOption({ language, ...rest }: LanguageOptionProps) {
       />
 
       <span>
-        {language.name}
+        {t(language.name)}
       </span>
     </button>
   );
