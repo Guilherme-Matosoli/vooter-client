db79ee5c9324a2200a252473d22858919a082a7d
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-09 16:12:16 -0300

    add itemPosition and maxitems props :wrench:

--- src/components/LanguageOption/index.tsx
+++ src/components/LanguageOption/index.tsx
@@ -8,16 +8,18 @@ interface Language {
 };
 
 interface LanguageOptionProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  language: Language
+  language: Language,
+  position: number,
+  maxItems: number
 };
 
 export function LanguageOption({ language, ...rest }: LanguageOptionProps) {
   const { t } = useTranslation(i18next.language, "languages");
   return (
-    <button {...rest} className="w-full h-8 hover:bg-gray-200 transition flex items-center gap-3 p-3">
+    <button {...rest} className="w-full transition flex items-center gap-2 border-b border-gray-300 py-3">
       <img
         src={language.flag}
-        className="w-6 h-6 rounded-full"
+        className="w-7 h-7 rounded-full"
       />
 
       <span>
