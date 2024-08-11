48a03f9a41ff6aabeeafeb9903088c24b25e464b
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-10 23:50:53 -0300

    pass position as relative :hammer:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,8 +11,8 @@ export function Header({ lng }: HeaderProps) {
   const matches = useMediaQuery(768);
 
   return (
-    <header className="flex justify-between items-center w-full bg-white dark:bg-black py-4 px-5 rounded-lg">
-      <h1 className="font-main font-bold text-2xl text-light dark:text-dark">
+    <header className="relative flex justify-between items-center w-full bg-white dark:bg-black py-4 px-5 rounded-lg">
+      <h1 className="z-10 font-main font-bold text-2xl text-light dark:text-dark">
         <a href="/">
           Votter
         </a>
