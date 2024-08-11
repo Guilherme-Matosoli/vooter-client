c1e3d150113d5c7046dae726472a52d4b29cc81f
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-11 17:31:48 -0300

    finish footer component :tada:

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useTranslation } from "@/app/i18n";
+import i18next from "i18next";
+
+export function Footer() {
+  const { t } = useTranslation(i18next.language, "footer");
+  return (
+    <footer className="w-full flex justify-center">
+      <span className="font-main text-secondary text-lg">
+        &copy; {new Date().getFullYear()} Votter. {t("copy")}
+      </span>
+    </footer>
+  );
+};
