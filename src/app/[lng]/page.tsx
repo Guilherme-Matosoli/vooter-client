b8eb3c0dd0514180fb1e3d6763e41f73fccc508d
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-11 20:03:57 -0300

    add a wrapper to group main text and secondary text :rocket:

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -11,8 +11,22 @@ interface HomeParams {
 export default function Home({ params: { lng } }: HomeParams) {
   const { t } = useTranslation(lng, "home");
   return (
-    <main className="content">
-      <Header lng={lng} />
+    <main className="flex flex-1 flex-col items-center justify-center">
+      <div className="w-full flex flex-col gap-5">
+        <h2 className="font-main font-bold text-center text-4xl dark:text-white md:text-6xl">
+          {t("mainText")}
+        </h2>
+
+        <h3 className="font-main font-bold text-center text-secLight dark:text-secDark">
+          {t("secondaryText")}
+        </h3>
+      </div>
+
+      <img
+        src="/ilustration.svg"
+        alt={t("ilustration")}
+      />
+
     </main>
   );
 }
