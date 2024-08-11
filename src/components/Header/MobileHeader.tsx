0e3751a076d5633e364186cd868c5b3331297b16
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-11 17:45:10 -0300

    change mobile header menu button in dark theme :rocket:

--- src/components/Header/MobileHeader.tsx
+++ src/components/Header/MobileHeader.tsx
@@ -22,7 +22,7 @@ export function MobileHeader({ lng }: HeaderProps) {
     <section ref={elementRef} className="flex items-center h-full">
       <button
         onClick={() => setOpen(!open)}
-        className="z-10 flex items-center justify-center h-[45px] w-[45px] rounded-full hover:bg-gray-200"
+        className="z-10 flex items-center justify-center h-[45px] w-[45px] rounded-full hover:bg-gray-200 dark:hover:bg-gray-900"
       >
         <span className={`menuIcon ${open && "open"}`}></span>
       </button>
