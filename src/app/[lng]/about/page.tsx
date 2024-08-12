57d5a058ca6bb4527a972ac39a0d06e315fe5b8b
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-12 12:12:39 -0300

    add title and description styles :tada:

--- src/app/[lng]/about/page.tsx
+++ src/app/[lng]/about/page.tsx
@@ -8,11 +8,17 @@ interface AboutParams {
 }
 
 export default function About({ params: { lng } }: AboutParams) {
-  const { t } = useTranslation(lng, "about");
+  const { t } = useTranslation(lng, "aboutPage");
 
   return (
-    <main className="flex flex-col items-center py-20">
-      <h2></h2>
+    <main className="flex flex-col py-20">
+      <h2 className="font-main font-bold text-4xl dark:text-white md:text-6xl">
+        {t("mainText")}
+      </h2>
+
+      <p className="font-main font-normal md:text-2xl">
+        {t("description")}
+      </p>
     </main>
   );
 };
