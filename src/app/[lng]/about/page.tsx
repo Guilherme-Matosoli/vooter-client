fcd2c31fa771a389e4626d576765f1fa54a0a94c
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-12 12:03:53 -0300

    configure translation hook in about page :tada:

--- src/app/[lng]/about/page.tsx
+++ src/app/[lng]/about/page.tsx
@@ -1,7 +1,18 @@
-export default function About() {
+"use client";
+import { useTranslation } from "@/app/i18n";
+
+interface AboutParams {
+  params: {
+    lng: string
+  }
+}
+
+export default function About({ params: { lng } }: AboutParams) {
+  const { t } = useTranslation(lng, "about");
+
   return (
-    <main>
-      <h2>capeta</h2>
+    <main className="flex flex-col items-center py-20">
+      <h2></h2>
     </main>
   );
 };
