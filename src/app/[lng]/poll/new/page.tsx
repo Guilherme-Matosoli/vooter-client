6a73bafff945333553852730fc4dda80bc2e2e4f
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-12 16:37:51 -0300

    configure translation hook :robot:

--- src/app/[lng]/poll/new/page.tsx
+++ src/app/[lng]/poll/new/page.tsx
@@ -1,7 +1,18 @@
-export default function NewPoll() {
+"use client";
+import { useTranslation } from "@/app/i18n";
+
+interface NewPollParams {
+  params: {
+    lng: string
+  };
+};
+
+export default function NewPoll({ params: { lng } }: NewPollParams) {
+  const { t } = useTranslation(lng, "createPoll");
   return (
-    <main className="flex flex-1 flex-col py-20">
-      <h2>
+    <main className="flex flex-1 items-center flex-col py-20">
+      <h2 className="font-main font-bold text-4xl dark:text-white md:text-6xl">
+        {t("createNewPoll")}
       </h2>
     </main>
   );
