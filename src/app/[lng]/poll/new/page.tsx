7e5e87e68c2829af2daf4ac6ed297ff161f0b127
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-13 17:13:37 -0300

    remove options list to add it in a context :tada:

--- src/app/[lng]/poll/new/page.tsx
+++ src/app/[lng]/poll/new/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 import { useTranslation } from "@/app/i18n";
 import { Input } from "@/components/Input";
-import { useState } from "react";
-
-interface Options {
-  [key: string]: string
-};
 
 interface NewPollParams {
   params: {
@@ -15,12 +10,6 @@ interface NewPollParams {
 
 export default function NewPoll({ params: { lng } }: NewPollParams) {
   const { t } = useTranslation(lng, "createPoll");
-
-  const [options, setOptions] = useState<Options[]>([
-    {
-      option: "Opção 1"
-    }
-  ]);
 
   return (
     <main className="flex flex-1 items-center flex-col py-20">
