30a32744a6494a0f77b5172bd78325844f89cdcd
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-14 16:30:25 -0300

    remove poll layout :tada:

--- src/app/[lng]/poll/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PollContextProvider } from "@/contexts/PollContext";
-
-export default function PollLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <PollContextProvider>
-      {children}
-    </PollContextProvider>
-  );
-};
