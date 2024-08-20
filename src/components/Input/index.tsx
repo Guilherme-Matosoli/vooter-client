e80c09e4b56ca2b0d87c283189ada72b3d1f54d1
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-20 14:58:25 -0300

    add hasDelButton property and refactor props inside component :rocket:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,25 +1,25 @@
 import { forwardRef, InputHTMLAttributes, Ref } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string
+  label: string,
+  hasDelButton?: boolean;
 };
 
 export const Input = forwardRef((props: InputProps, ref: Ref<HTMLInputElement>) => {
-  const { label, ...rest } = props;
   return (
     <section className="w-full flex flex-col gap-1">
       <label
-        htmlFor={rest.name}
+        htmlFor={props.name}
         className="font-main font-semibold text-2xl dark:text-white"
       >
-        {label}
+        {props.label}
       </label>
 
       <input
         className="font-main outline-none p-5 rounded-lg border border-black
         bg-white dark:bg-black dark:text-white dark:border-white"
-        {...rest}
-        id={rest.name}
+        {...props}
+        id={props.name}
         ref={ref}
       />
     </section >
