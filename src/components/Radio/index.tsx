d2da8a0cca8abf9922439ea3b7aee22fbf166a73
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-21 16:36:23 -0300

    :sparkles: feat(components/Radio): Add custom radio button component with click simulation and styles.

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -1,14 +1,28 @@
-import { InputHTMLAttributes } from "react";
+import "./styles.css";
+import { InputHTMLAttributes, useRef } from "react";
 
 interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
 
 };
 
 export function Radio({ ...props }: RadioProps) {
+  const radioRef = useRef<HTMLInputElement>(null);
+
+  function simulateClick() {
+    radioRef.current!.click()
+  };
+
   return (
-    <input
-      type="radio"
-      {...props}
-    />
+    <section onClick={simulateClick} className="container relative w-[30px] h-[30px]">
+      <input
+        type="radio"
+        {...props}
+        ref={radioRef}
+        hidden
+      />
+
+      <span className="check w-[30px] h-[30px] absolute top-0 left-0 bg-light rounded-full border border-black">
+      </span>
+    </section>
   )
 }
