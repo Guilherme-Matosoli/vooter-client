dee4a6f3dcd3ce2c6cdd2474669c0ea50ab34c2b
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-21 14:57:19 -0300

    :sparkles: feat(poll/preview): Add translation and context support to PollPreview component

--- src/app/[lng]/poll/preview/page.tsx
+++ src/app/[lng]/poll/preview/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useTranslation } from "@/app/i18n";
+import { PollContext } from "@/contexts/PolllContext";
+import { useContext } from "react";
 
 interface PollPreviewProps {
   params: {
@@ -6,8 +11,21 @@ interface PollPreviewProps {
 };
 
 export default function PollPreview({ params: { lng } }: PollPreviewProps) {
+  const { t } = useTranslation(lng, "pollPreview");
+
+  const { fields } = useContext(PollContext);
+
   return (
-    <main className="flex flex-1 flex-col items-center justify-center py-5">
+    <main className="flex flex-1 flex-col gap-5 items-center justify-center py-5">
+      <h1 className="font-main font-bold text-4xl dark:text-white md:text-6xl">
+        {t("mainTitle")}
+      </h1>
+
+      <section className="w-full flex flex-col flex-1 bg-white p-5 rounded-lg">
+        <h2>
+          {fields.title || "Building"}
+        </h2>
+      </section>
     </main>
-  )
-}
+  );
+};
