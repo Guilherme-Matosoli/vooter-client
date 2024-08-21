cee02c6bf15dbe0a8740a0d817cf7637bba86502
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-21 16:38:26 -0300

    :lipstick: style(components/Option): Update styles for dark mode support in `Option` component.

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -3,9 +3,9 @@ import { Radio } from "../Radio";
 export function Option() {
   return (
     <div className="w-full min-h-24 flex items-center justify-between border border-black 
-                    rounded-lg p-4"
+      rounded-lg p-4 dark:bg-black dark:border-white"
     >
-      <span className="font-main font-semibold text-2xl">
+      <span className="font-main font-semibold text-2xl dark:text-white">
         Option 1
       </span>
 
