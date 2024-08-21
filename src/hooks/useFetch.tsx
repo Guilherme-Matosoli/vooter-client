5b4ed5e3e22442772df39a132c765aa7a784beb1
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-21 18:46:06 -0300

    :sparkles: feat(hooks/useFetch): Enhance `useFetch` hook to include an execute function and improved error handling.

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -6,19 +6,36 @@ interface UseFetchProps {
   method: "get" | "post" | "put" | "delete"
 };
 
-type UseFetchReturn<T> = [T, boolean, Error];
+type UseFetchReturn<T> = [
+  execute: () => Promise<void>,
+  T,
+  boolean,
+  Error | null
+];
 
 const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
 
 export function useFetch<T>({ path, method }: UseFetchProps): UseFetchReturn<T> {
+  const [error, setError] = useState<unknown>();
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState();
 
-  api.request({
-    url: path,
-    method
-  })
-    .then(response => setData(response.data));
+  async function execute() {
+    setLoading(true);
 
-  return [data as T, loading, new Error("")];
+    try {
+      const response = await api.request({
+        url: path,
+        method
+      });
+
+      setData(response.data);
+    }
+    catch (err) {
+      setError(err);
+    }
+    finally { setLoading(false) };
+  };
+
+  return [execute, data as T, loading, error as Error];
 };
