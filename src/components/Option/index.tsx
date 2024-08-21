52c03b293b6df9b99a4643e5b3e876814b4d0c46
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-21 16:37:31 -0300

    :lipstick: style(components/Option): Enhance styling of `Option` component with improved layout and design.

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,7 +1,15 @@
+import { Radio } from "../Radio";
 
 export function Option() {
   return (
-    <div>
+    <div className="w-full min-h-24 flex items-center justify-between border border-black 
+                    rounded-lg p-4"
+    >
+      <span className="font-main font-semibold text-2xl">
+        Option 1
+      </span>
+
+      <Radio />
     </div>
   );
 };
