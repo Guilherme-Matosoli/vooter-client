ea8761a80e0c8ec1a09c0aa94ff3fcdff1e4fc4b
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-21 17:00:43 -0300

    :sparkles: feat(components/Option): Add option prop to `Option` component for dynamic rendering.

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,12 +1,16 @@
 import { Radio } from "../Radio";
 
-export function Option() {
+interface OptionProps {
+  option: string
+};
+
+export function Option({ option }: OptionProps) {
   return (
     <div className="w-full min-h-24 flex items-center justify-between border border-black 
       rounded-lg p-4 dark:bg-black dark:border-white"
     >
-      <span className="font-main font-semibold text-2xl dark:text-white">
-        Option 1
+      <span className="block font-main font-semibold text-2xl break-words dark:text-white w-2/3 h-auto">
+        {option}
       </span>
 
       <Radio />
