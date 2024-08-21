d243cf1d9c25a9226e198c183ea8052397484886
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-21 17:02:34 -0300

    :lipstick: style(components/Option): Update `Option` component to use a label for better accessibility and associate the radio input with the option.

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,19 +1,21 @@
 import { Radio } from "../Radio";
 
 interface OptionProps {
+  id: string,
   option: string
 };
 
-export function Option({ option }: OptionProps) {
+export function Option({ option, id }: OptionProps) {
   return (
-    <div className="w-full min-h-24 flex items-center justify-between border border-black 
+    <label className="w-full min-h-24 flex items-center justify-between border border-black 
       rounded-lg p-4 dark:bg-black dark:border-white"
+      htmlFor={id}
     >
       <span className="block font-main font-semibold text-2xl break-words dark:text-white w-2/3 h-auto">
         {option}
       </span>
 
-      <Radio />
-    </div>
+      <Radio id={id} name="option" />
+    </label>
   );
 };
