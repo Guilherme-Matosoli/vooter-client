8e6cf03b7a07d00a67de4c92633fa8239f6d30db
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-21 14:29:22 -0300

    add params in pollpreview page :tada:

--- src/app/[lng]/poll/preview/page.tsx
+++ src/app/[lng]/poll/preview/page.tsx
@@ -1,6 +1,11 @@
 
+interface PollPreviewProps {
+  params: {
+    lng: string;
+  };
+};
 
-export default function PreviewPoll() {
+export default function PollPreview({ params: { lng } }: PollPreviewProps) {
   return (
     <main className="flex flex-1 flex-col items-center justify-center py-5">
     </main>
