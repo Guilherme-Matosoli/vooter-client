7839d44c2a506078b8dd9af7aa63875f8443226b
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-23 19:16:04 -0300

    :lipstick: style(poll/questions): Enhance styling and layout of the questions form in the poll component.

--- src/app/[lng]/poll/[id]/questions.tsx
+++ src/app/[lng]/poll/[id]/questions.tsx
@@ -2,15 +2,29 @@
 import { useTranslation } from "@/app/i18n";
 import { Poll } from "./page";
 import i18n from "i18next"
+import { Option } from "@/components/Option";
 
 export function Questions({ questions, id }: Pick<Poll, "questions" | "id">) {
   const lng = i18n.language;
   const { t } = useTranslation(lng, "pollPage");
 
   return (
-    <form>
+    <form className="w-full flex flex-col items-center">
 
-      <button>
+      {
+        questions.map(question => {
+          return (
+            <Option
+              id={question.id}
+              option={question.option}
+            />
+          )
+        })
+      }
+
+      <button className="button bg-black text-white dark:bg-white 
+        dark:border dark:border-black dark:text-black md:w-1/3"
+      >
         {t("confirm")}
       </button>
     </form>
