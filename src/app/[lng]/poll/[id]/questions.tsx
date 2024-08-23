841e0946558c376ac7956baba71b9823c55ca152
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-23 19:08:42 -0300

    :sparkles: feat(poll/questions): Add translation support for the confirm button in the Questions component.

--- src/app/[lng]/poll/[id]/questions.tsx
+++ src/app/[lng]/poll/[id]/questions.tsx
@@ -1,8 +1,18 @@
+"use client";
+import { useTranslation } from "@/app/i18n";
 import { Poll } from "./page";
+import i18n from "i18next"
 
 export function Questions({ questions }: Pick<Poll, "questions">) {
+  const lng = i18n.language;
+  const { t } = useTranslation(lng, "pollPage");
+
   return (
     <form>
+
+      <button>
+        {t("confirm")}
+      </button>
     </form>
   );
 };
