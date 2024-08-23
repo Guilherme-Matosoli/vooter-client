fe94db75eb0805bf1da451e0503ef07e993ba4d1
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-23 13:20:05 -0300

    :sparkles: feat(poll): Fetch and display poll title in PollPage component.

--- src/app/[lng]/poll/[id]/page.tsx
+++ src/app/[lng]/poll/[id]/page.tsx
@@ -1,20 +1,24 @@
+import { api } from "@/services/api";
+
 interface Poll {
   id: string,
+  title: string,
   questions: { id: string, option: string }[]
 };
 
 interface PollPageProps {
   params: {
-    lng: string,
     id: string
   };
 };
 
-export default async function PollPage({ params: { lng, id } }: PollPageProps) {
+export default async function PollPage({ params: { id } }: PollPageProps) {
+  const req = await api.get<Poll>(`/poll/${id}`);
+  const poll = req.data;
 
   return (
     <main className="flex flex-1 flex-col gap-5 items-center justify-center py-5">
-      <h1></h1>
+      <h1>{poll.title}</h1>
     </main>
   )
 };
