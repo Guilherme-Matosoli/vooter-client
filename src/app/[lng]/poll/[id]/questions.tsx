14c6ec1dba87cb78442fb742943d54ea7dec20c5
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-23 22:45:08 -0300

    :sparkles: feat(poll/loading): Add loading component and integrate form handling in questions component

--- src/app/[lng]/poll/[id]/questions.tsx
+++ src/app/[lng]/poll/[id]/questions.tsx
@@ -3,21 +3,32 @@ import { useTranslation } from "@/app/i18n";
 import { Poll } from "./page";
 import i18n from "i18next"
 import { Option } from "@/components/Option";
+import { useForm } from "react-hook-form";
 
 export function Questions({ questions, id }: Pick<Poll, "questions" | "id">) {
   const lng = i18n.language;
   const { t } = useTranslation(lng, "pollPage");
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      voted_in: ""
+    }
+  });
 
   return (
-    <form className="w-full flex flex-col items-center gap-6">
+    <form
+      className="w-full flex flex-col items-center gap-6"
+      onSubmit={handleSubmit(e => console.log(e))}
+    >
       <section className="w-full flex flex-col gap-2">
         {
           questions.map(question => {
             return (
               <Option
+                key={question.id}
                 id={question.id}
                 option={question.option}
                 value={question.id}
+                {...register("voted_in")}
               />
             )
           })
