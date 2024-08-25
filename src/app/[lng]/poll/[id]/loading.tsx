196b16b114f04923837cfea19dd5e8f0e3f5eed3
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-08-24 23:26:44 -0300

    :lipstick: style(poll/loading): Enhance loading component with improved styling and animation.

--- src/app/[lng]/poll/[id]/loading.tsx
+++ src/app/[lng]/poll/[id]/loading.tsx
@@ -1,5 +1,13 @@
+
 export default function Loading() {
   return (
-    <h1>loading..</h1>
+    <main className="flex gap-5 flex-1 flex-col items-center justify-center animate-pulse">
+      <div className="w-2/3 h-14 bg-gray-300 rounded-lg md:h-16 dark:bg-gray-600"></div>
+
+      <div className="w-full h-28 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+      <div className="w-full h-28 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+
+      <div className="w-full h-14 bg-gray-300 rounded-lg md:w-1/3 dark:bg-gray-600"></div>
+    </main>
   )
 };
